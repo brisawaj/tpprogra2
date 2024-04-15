98028511de9ab97d04b5ef83ee09d5454276b878
brisawaj <bwajsbrot@udesa.edu.ar>
2024-04-15 17:41:03 -0300

    ruta en views

--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,3 @@
-const express = require (‘express’);
-const router = express.Router();
 
 
-router.get(‘/’, function (req, res){
-	return res.send(‘hola!’)
-})
 
-
-router.get (‘/remera/:1', function(req,res){
-    let resultado = [];
-    let autoModel = req.params.model;
-
-    for (let i = 0; i < db.lista.length; i++) {
-    if(autoModel == db.lista[i].modelo){
-        resultado.push(db.lista[i])
-    }        
-    }
-    if(resultado.length == 0){
-        return res.send('no tenemos modelo ' + autoModel)
-    }else{
-        return res.send(resultado)
-    }
-module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/id/:id', productsController.filterById)
+router.get('/register', productsController.filterByRegister)
+router.get('/login', productsController.filterByLogin)
+
+
+
 module.exports = router;
