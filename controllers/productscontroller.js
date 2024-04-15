44ac0305bbf19fa6b9724fed20cf13679c33f40a
Sol Orsi <sorsifernandez@udesa.com.ar>
2024-04-15 18:11:50 -0300

    controller

--- controllers/productscontroller.js
+++ controllers/productscontroller.js
@@ -1,3 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const db = require("../db/index");
+const db = require('../db/index')
+const productController = {
+    index: function (req, res) {
+        return res.send(db.lista)}
+    },
+    
+    filterById: function (req, res) {
+        let ropaId = req.params.marca;
+        let resultado = [];
+        for (let i = 0; i < db.lista.length; i++) {
+            if (ropaId == db.lista[i].id) {
+                resultado.push(db.lista[i])
+            }
+        }
+        if (resultado.length == 0) {
+            return res.send('no tenemos ningun producto con id ' + ropaId)
+        }
+        else {
+            return res.render('products', {
+                index: resultado;
+                mensaje:'Productos con ese id'                
+            })
+        }
+    },
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController')
