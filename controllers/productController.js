9abc5797db69abdaa67199822192608a12e00a0a
Sol Orsi <sorsifernandez@udesa.com.ar>
2024-04-15 23:31:02 -0300

    controllers

---
+++ controllers/productController.js
@@ -0,0 +1,25 @@
+const db = require('../db/index')
+
+const productController = {
+    
+    index: function (req, res) {
+        return res.render("index")
+    },
+
+    filterById: function (req, res) {
+       
+        return res.render('product', {
+            index: resultado,
+            mensaje:'Productos con ese id'                
+        })
+    },
+    search: function (req, res) {
+       
+        return res.render("search-results")
+    },
+ }
+    
+    
+
+module.exports = productController;
+
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-router.get('/id/:id', productsController.filterById)
-router.get('/register', productsController.filterByRegister)
-router.get('/login', productsController.filterByLogin)
+//router.get('/id/:id', productController.filterById)
+//router.get('/register', productController.filterByRegister)
+//router.get('/login', productController.filterByLogin)
 
 
 
