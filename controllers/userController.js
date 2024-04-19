0de4047f7956ecc2d369bc57b06be9e201ef30e2
Sol Orsi <sorsifernandez@udesa.com.ar>
2024-04-19 15:29:07 -0300

    formulario login

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,12 @@ const userController = {
     
     register: function(req, res) {
         return res.render('register')
-    } 
+    },
+    
+    login: function(req, res) {
+        return res.render('login')
+    }  
+
  }
     
     
