b1f28e3caafa5f03219ba4c1b7b11a657384ce44
brisawaj <bwajsbrot@udesa.edu.ar>
2024-06-22 20:37:24 -0300

    editar,borrar producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,9 +8,10 @@ const productController = {
 
     filterById: function (req, res) {
         // Obtenemos la ID de los query parameters 
-        const filtro= {include: [{association: "vendedor"}]}
+        const filtro= {include: [{association: "vendedor"},{association: "comentarios"}]}
         Product.findByPk(req.params.id, filtro)
         .then(function(producto){
+            //return res.send(producto)
             return res.render('product', {
                 producto: producto
             });  
@@ -22,6 +23,38 @@ const productController = {
     productAdd: function (req, res) {
         return res.render("productAdd", { usuario: db.usuario, productos: db.producto })
     },
+
+    productEdit: function (req, res) {
+        Product.findByPk(req.params.id)
+        .then(function(producto){
+            //return res.send (producto)
+            return res.render("productEdit", { usuario: db.usuario, producto: producto })    
+        })
+        
+    
+    },
+
+    update: function(req, res){
+        console.log(req.body)
+        let producto= {
+            imagen: req.body.image,
+            nombre_producto: req.body.name,
+            descripcion_producto: req.body.description,
+            updatedAt: new Date ()
+        }
+        Product.update(producto, { where:{id:req.params.id}})
+        .then (function(){
+            return res.redirect("/products/id/"+req.params.id)
+        })
+     },
+
+     destroy: function(req, res){
+        console.log(req.body)
+        Product.destroy({ where:{id:req.params.id}})
+        .then (function(){
+            return res.redirect("/")
+        })
+     },
     
     create: function(req, res){
         console.log(req.body)
--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,7 @@ router.get('/id/:id', productController.filterById)
 router.get('/search', productController.search)
 router.get('/productAdd', productController.productAdd)
 router.post('/productAdd',productController.create)
+router.get('/productEdit/:id', productController.productEdit)
+router.post('/productEdit/:id',productController.update)
+router.get('/productDelete/:id',productController.destroy)
 module.exports = router;
